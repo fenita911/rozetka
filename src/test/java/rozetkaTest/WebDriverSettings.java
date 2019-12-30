7f6867c36b6861759c7a197526909464f1b95c32
nataliia.moroz <fenita911@gmail.com>
2019-12-30 14:45:36 +0200

    REFACTOR DRIVER FOR UNIX

--- src/test/java/rozetkaTest/WebDriverSettings.java
+++ src/test/java/rozetkaTest/WebDriverSettings.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.io.File;
 import java.io.IOException;
 
 public class WebDriverSettings {
@@ -15,7 +14,7 @@ public class WebDriverSettings {
 
     @Before
     public void setUp () throws IOException {
-        System.setProperty("webdriver.chrome.driver", new File("src"+File.separator+"main"+File.separator+"resources"+File.separator+"chromedriver").getAbsolutePath());
+        System.setProperty("webdriver.chrome.driver", getClass().getClassLoader().getResource("chromedriver").getPath());
 //        System.setProperty("webdriver.chrome.driver", "/home/nataliiamoroz/G29Project/rozetka/src/main/resources/chromedriver");
 
         //System.setProperty("webdriver.gecko.driver", getClass().getClassLoader().getResource("geckodriver").getPath());
